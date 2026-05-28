c99bc5c0b8e8f45d6bed1a301ee2680de1bc1ae7
Vega Nicholas S. <veganicholas123@gmail.com>
2026-05-29 05:09:17 +0700

    fix(v1001): correct BossEvent field serialization and add player actor fallback

--- src/Codec/v1001/BossEventCodec.php
+++ src/Codec/v1001/BossEventCodec.php
@@ -10,10 +10,10 @@ use Nicholass003\Axiom\Codec\CodecType;
 use Nicholass003\Axiom\Enum\BossEventType;
 use Nicholass003\Axiom\Packet\BossEventPacket;
 use Nicholass003\Axiom\Packet\Packet;
+use pmmp\encoding\Byte;
 use pmmp\encoding\ByteBufferReader;
 use pmmp\encoding\ByteBufferWriter;
 use pmmp\encoding\LE;
-use pmmp\encoding\VarInt;
 
 class BossEventCodec implements Codec{
 
@@ -21,24 +21,24 @@ class BossEventCodec implements Codec{
         $pk = new BossEventPacket();
         $pk->bossActorUniqueId = CodecHelper::readActorUniqueId($in);
         $pk->playerActorUniqueId = CodecHelper::readActorUniqueId($in);
-        $pk->eventType = BossEventType::safe(VarInt::readUnsignedInt($in));
+        $pk->eventType = BossEventType::safe(Byte::readUnsigned($in));
         $pk->title = CodecHelper::readString($in);
         $pk->filteredTitle = CodecHelper::readString($in);
         $pk->healthPercent = LE::readFloat($in);
-        $pk->color = VarInt::readUnsignedInt($in);
-        $pk->overlay = VarInt::readUnsignedInt($in);
+        $pk->color = Byte::readUnsigned($in);
+        $pk->overlay = Byte::readUnsigned($in);
         return $pk;
     }
 
     public function encode(ByteBufferWriter $out, Packet $pk, CodecType $codec) : void{
         assert($pk instanceof BossEventPacket);
         CodecHelper::writeActorUniqueId($out, $pk->bossActorUniqueId);
-        CodecHelper::writeActorUniqueId($out, $pk->playerActorUniqueId);
-        VarInt::writeUnsignedInt($out, $pk->eventType->value);
+        CodecHelper::writeActorUniqueId($out, $pk->playerActorUniqueId ?? -1);
+        Byte::writeUnsigned($out, $pk->eventType->value);
         CodecHelper::writeString($out, $pk->title);
         CodecHelper::writeString($out, $pk->filteredTitle);
         LE::writeFloat($out, $pk->healthPercent);
-        VarInt::writeUnsignedInt($out, $pk->color);
-        VarInt::writeUnsignedInt($out, $pk->overlay);
+        Byte::writeUnsigned($out, $pk->color);
+        Byte::writeUnsigned($out, $pk->overlay);
     }
 }
